74ae15694c67ebbb8cc91aaa3e37f206d048e62c
bikashneupane17 <edubikkash@gmail.com>
2024-07-31 16:02:08 +1000

    Route to Delete images from express server

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ import express from "express";
 import cors from "cors";
 import multerUpload from "./src/utils/multer.js";
 import filePath from "path";
+import fs from "fs";
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-const _dirname = filePath.resolve();
-app.use(express.static(filePath.join(_dirname, "public/images")));
+const __dirname = filePath.resolve();
+console.log(filePath.join(__dirname, "public/images"));
+app.use(express.static(filePath.join(__dirname, "public/images")));
 
+// upload images
 app.post(
   "/api/v1/upload-image",
   multerUpload.array("images", 5),
@@ -23,7 +26,8 @@ app.post(
       // generate image urls from express server
       if (req.files.length > 0) {
         const imgeUrls = req.files.map((file) => {
-          const filePath = file.path.replace("public", "");
+          console.log(file);
+          const filePath = file.path.replace("public/images", "");
           return filePath;
         });
 
@@ -46,6 +50,35 @@ app.post(
     }
   }
 );
+
+// remove images from express server
+app.delete("/api/v1/delete-images", (req, res) => {
+  try {
+    const { imagePath } = req.body;
+
+    // delete images
+    if (imagePath?.length > 0) {
+      imagePath.forEach((item) => {
+        fs.unlinkSync(filePath.join("public/images", item), (error) => {
+          return res.status(400).json({
+            status: "error",
+            message: "Invalid or missing imagePath",
+          });
+        });
+      });
+    }
+    res.json({
+      status: "success",
+      message: "Images Deleted",
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error,
+    });
+  }
+});
+
 app.listen(8000, (error) =>
   error ? console.log(error) : console.log("Server Alive")
 );
--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,20 +1,37 @@
 import multer from "multer";
 
-//storage
+// storage
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images"); // destination folder
+    cb(null, "public/images"); // destination folder path
   },
   filename: (req, file, cb) => {
     cb(null, `${new Date()}-${file.originalname}`); // unique path name
   },
 });
 
-//limits
+// limits
 const limits = {
   fieldSize: 10 * 1024 * 1024,
 };
 
+// filters
+const multerFilters = (req, file, cb) => {
+  const allowedMimeTypes = ["image/jpeg", "image/png"];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    // file type is acceptable
+    cb(null, true);
+  } else {
+    const err = new Error("Invalid file type, Only JPWG and PNG allowed.");
+    cb(err, false);
+  }
+};
+
 // export multer
-const multerUpload = multer({ storage: multerStorage }, { limits });
+const multerUpload = multer({
+  storage: multerStorage,
+  limits,
+  fileFilter: multerFilters,
+});
 export default multerUpload;
